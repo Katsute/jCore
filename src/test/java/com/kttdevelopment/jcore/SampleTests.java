1426bc149d7d3561a2baf311a74b3a0f389a66b1
Katsute <58778985+Katsute@users.noreply.github.com>
2021-06-11 21:56:35 -0400

    Update SampleTests.java

--- src/test/java/com/kttdevelopment/jcore/SampleTests.java
+++ src/test/java/com/kttdevelopment/jcore/SampleTests.java
@@ -6,14 +6,14 @@ public final class SampleTests {
 
     @Test
     public void testJUnit(){
-        Assumptions.assumeTrue(System.getenv("sample-only") != null && System.getenv("sample-only").equals("false"));
+        Assumptions.assumeTrue(System.getenv("sample_only") != null && System.getenv("sample_only").equals("false"));
 
         Assertions.assertEquals(1 + 1, 3, "Expected 1 + 1 to equal 2");
     }
 
     @Test
     public void testJCore(){
-        Assumptions.assumeTrue(System.getenv("sample-only") != null && System.getenv("sample-only").equals("true"));
+        Assumptions.assumeTrue(System.getenv("sample_only") != null && System.getenv("sample_only").equals("true"));
 
         Assertions.assertEquals(1 + 1, 3, Workflow.errorSupplier("Expected 1 + 1 to equal 2"));
     }
